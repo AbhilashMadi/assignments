a9df49fd28597fddbc3fa31314ab3c6c214a705c
Abhilash Madi <madiabhilash1563@gmail.com>
2023-03-21 17:01:40 +0530

    add more fileds

--- src/components/InviteMember.jsx
+++ src/components/InviteMember.jsx
@@ -1,66 +1,70 @@
 import React, { useState } from 'react'
 
 function InviteMember() {
-  const [invites,setInvites] = useState([{userMail:"",userType:"",id:""}]);
+  const [invites, setInvites] = useState([
+    { id: Date.now(), mail: "", type: "" }
+  ]);
 
-  const handleAddMore = () => {
-    setInvites([...invites,{userMail:"",userType:"",id:""}])
-  }
+    function handleInputChange(e, index) {
+      const { name, value } = e.target;
+      const newInvites = [...invites];
+      newInvites[index][name] = value;
+      setInvites(newInvites);
+    }
 
-  const handleInputChange = (e,index) => {
-    const {name,value} = e.target;
-    const newInvites = [...invites];
-    newInvites[index][name] = value;
-    setInvites(newInvites);
-  }
+    function handleSelectChange(e, index) {
+      const { value } = e.target;
+      const newInvitees = [...invites];
+      newInvitees[index].type = value;
+      setInvites(newInvitees);
+    }
 
-  const handleSelectChange = (e,index) => {
-    const newInvites = [...invites];
-    newInvites[index].type = e.target.value;
-    setInvites(newInvites);
-  };
+    function handleAddMore() {
+      setInvites([...invites, { id: Date.now(),mail: "", type: "" }]);
+    }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(invites);
-  }
+    function handleSendInvite() {
+      console.log(invites);
+      // TODO: send invite
+    }
 
   return (
     <section className="invite-form">
       <p>Invite your co-workers to collaborate on Cashwise.</p>
 
-      {
-        invites.map((invite,index) => {
-          return (
-            <div className="invite-form__addUser" key={index}>
-              <input
-                type="email"
-                autoComplete="off"
-                name="userMail"
-                placeholder="Email"
-                onChange={(e) => handleInputChange(e, index)}
-              />
-              <select
-                value={invite.type}
-                onChange={(e) => handleSelectChange(e, index)}>
-                <option value="MemberType" disabled selected>
-                  Select Member
-                </option>
-                <option value="Admin">Admin</option>
-                <option value="Manager">Manager</option>
-                <option value="Worker">Worker</option>
-                <option value="Other">Other</option>
-              </select>
-            </div>
-          );
-        })
-      }
+      {invites.map((invite, index) => {
+        return (
+          <div className="invite-form__addUser" key={index}>
+            <input
+              type="email"
+              autoComplete="off"
+              name="mail"
+              value={invite.mail}
+              placeholder="Email"
+              onChange={(e) => handleInputChange(e, index)}
+            />
+            <select
+              value={invite.type}
+              onChange={(e) => handleSelectChange(e, index)}>
+              <option value="MemberType" disabled>
+                Select Member
+              </option>
+              <option value="Admin">Admin</option>
+              <option value="Manager">Manager</option>
+              <option value="Worker">Worker</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+        );
+      })}
 
       <div className="buttons">
         <button className="buttons__add-more" onClick={handleAddMore}>
           + Add More
         </button>
-        <button className="buttons__send-invite" onClick={handleSubmit}>Send Invite</button>
+        <button className="buttons__send-invite" onClick={handleSendInvite}>
+          Send Invite
+        </button>
       </div>
     </section>
   );
