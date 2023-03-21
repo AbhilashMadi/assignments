828c74c22e6b8f264bddccae8b88f4ccc5b3522a
Abhilash Madi <madiabhilash1563@gmail.com>
2023-03-21 12:11:32 +0530

    sidebar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,59 @@
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
+
+import logo from "../assets/logo.svg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight,faFolder,faFolderPlus,faFolderOpen } from "@fortawesome/free-solid-svg-icons";
+
+function SideBar() {
+  const [isExpanded, setExpendState] = useState(true);
+  const menuItems = [
+    {
+      text: "Documenth Generation",
+      icon: faFolder
+    },
+    {
+      text: "Corporate Documents",
+      icon: faFolderOpen,
+    },
+    {
+      text: "Filing Documents",
+      icon: faFolderPlus,
+    },
+  ];
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar__container">
+        <Link to="/" className="sidebar__logo">
+          <img src={logo} alt="logo"/>
+        </Link>
+
+        <div className="sidebar__user">
+          <div className="avatar">G</div>
+          <div>
+            <h4 className="entity-name">Entity Name</h4>
+            <p className="website-name">www.website.com</p>
+          </div>
+          <button className="profile-btn">
+            <FontAwesomeIcon icon={faAngleRight} />
+          </button>
+        </div>
+
+        <nav className="sidebar__navigation">
+          {menuItems.map(({ text, icon }) => {
+            return (
+              <Link to="/" className="nav-item">
+                <FontAwesomeIcon icon={icon} />
+                {isExpanded && <p>{text}</p>}
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+    </aside>
+  );
+}
+
+export default SideBar;
