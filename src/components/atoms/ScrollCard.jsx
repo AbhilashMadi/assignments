f5f9ea53549296c88c51ca8151ef6ec58407d670
Abhilash Madi <madiabhilash1563@gmail.com>
2023-03-28 17:08:02 +0530

    slide responsiveness fix

--- src/components/atoms/ScrollCard.jsx
+++ src/components/atoms/ScrollCard.jsx
@@ -8,13 +8,39 @@ import "../../scss/css/scrollcard.css";
 function ScrollCard({query}) {
   const {state} = useContext(DataContext);
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-  };
+      var settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1.4,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+    };
+
   return (
     <section className='scroll-section'>
       <Slider {...settings}>
