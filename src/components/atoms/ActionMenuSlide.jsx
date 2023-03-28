7f381f40b144d87283e0d21aa5940f7a7bbb396e
Abhilash Madi <madiabhilash1563@gmail.com>
2023-03-28 12:58:01 +0530

    navbar completed

--- src/components/atoms/ActionMenuSlide.jsx
+++ src/components/atoms/ActionMenuSlide.jsx
@@ -8,28 +8,48 @@ function ActionMenuSlide({ showMenu, setShowMenu, toggleMenu }) {
   return (
     <nav className='action-menu'>
       <div className='action-menu__actions'>
-        <img src={logo} alt="zintlr news" />
+        <img src={logo} alt='zintlr news' />
         <button onClick={toggleMenu}>
           <img src={direction} alt='close' aria-label='close' />
         </button>
       </div>
-      <ul className="action-menu__list">
-          <li>
-            <Link to="#" className='action-menu__item'>item</Link>
-          </li>
-          <li>
-            <Link to="#" className='action-menu__item'>item</Link>
-          </li>
-          <li>
-            <Link to="#"className='action-menu__item'>item</Link>
-          </li>
-          <li>
-            <Link to="#" className='action-menu__item'>item</Link>
-          </li>
-          <li>
-            <Link to="#" className='action-menu__item'>item</Link>
-          </li>
+      <ul className='action-menu__list'>
+        <li className='action-menu__item'>
+          <i class='fa fa-film' aria-hidden='true'></i>
+          <Link className='link' to='#'>
+            Films
+          </Link>
+        </li>
+        <li className='action-menu__item'>
+          <i class='fa fa-rss' aria-hidden='true'></i>
+          <Link className='link' to='#'>
+            popular
+          </Link>
+        </li>
+        <li className='action-menu__item'>
+          <i class='fa fa-fire' aria-hidden='true'></i>
+          <Link className='link' to='#'>
+            trending
+          </Link>
+        </li>
+        <li className='action-menu__item'>
+          <i class='fa fa-graduation-cap' aria-hidden='true'></i>
+          <Link className='link' to='#'>
+            education
+          </Link>
+        </li>
       </ul>
+      <div className='action-menu__social' aria-label='social media links'>
+        <Link to='/' className='link'>
+          <i class='fa fa-facebook-square' aria-hidden='true'></i>
+        </Link>
+        <Link to='/' className='link'>
+          <i class='fa fa-instagram' aria-hidden='true'></i>
+        </Link>
+        <Link to='/' className='link'>
+          <i class='fa fa-twitter-square' aria-hidden='true'></i>
+        </Link>
+      </div>
     </nav>
   );
 }
