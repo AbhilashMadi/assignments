b2edbab6b44f8c2ee63408010154f406179545a9
Abhilash Madi <madiabhilash1563@gmail.com>
2023-03-29 09:15:20 +0530

    feed cards completed

--- src/components/atoms/FeedCard.jsx
+++ src/components/atoms/FeedCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 import {getTimeLine} from "../../helpers/export";
+import "../../scss/css/feedcard.css";
 
 function FeedCard({articles}) {
   return (
@@ -28,22 +30,23 @@ function Card({article}){
         urlToImage,
       } = article;
   return (
-    <div className='feed-card'>
-      {/* <i className='fa fa-bookmark'></i> */}
-      <div className='feed-card__content'>
-        <p>{getTimeLine(publishedAt)}</p>
-        <h4>{content}</h4>
-        <p>{description}</p>
-        <p>
-          <span>{author}</span>
-          <i className='fa fa-circle'></i>
-          <span>{source.name}</span>
-        </p>
+    <Link to={url} className='link'>
+      <div className='feed-card'>
+        {/* <i className='fa fa-bookmark'></i> */}
+        <div className='feed-card__content'>
+          <p className='feed-card__timeline'>{getTimeLine(publishedAt)}</p>
+          <h4 className='feed-card__heading'>{content}</h4>
+          <p className='feed-card__description'>{description}</p>
+          <p className='feed-card__details'>
+            <span className='author'>{author}</span>
+            <i className='fa fa-circle'></i>
+            <span className='source'>{source.name}</span>
+          </p>
+        </div>
+        <div className='feed-card__img' style={{backgroundImage:`url(${urlToImage})`}}>
+        </div>
       </div>
-       <div className='feed-card__img'>
-        <img src={urlToImage} alt={title}/>
-       </div>
-    </div>
+    </Link>
   );
 }
 
